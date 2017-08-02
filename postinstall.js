719a450a51988f10489cad8cdba2d0fb696976b8
MasGaNo <masgano@gmail.com>
2017-08-02 12:46:49 +0200

    Fix install deployement

--- postinstall.js
+++ postinstall.js
@@ -1,7 +1,9 @@
-var copy = require('copy');
+try {
+    var copy = require('copy');
 
-copy('./typings/backbone/index.d.ts', './node_modules/@types/backbone/', {flatten: true}, function(err, files) {
-    if (err) {
-        throw err;
-    }
-});
+    copy('./typings/backbone/index.d.ts', './node_modules/@types/backbone/', { flatten: true }, function (err, files) {
+        if (err) {
+            throw err;
+        }
+    });
+} catch (e) { }
